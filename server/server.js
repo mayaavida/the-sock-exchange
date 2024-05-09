7b41f99239b19fb4903b821553f67d04c5b836bf
wasadmin <mayampeters@gmail.com>
2024-05-09 13:34:38 -0400

    sets up server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,109 @@
-console.log('Hello, world!');
+import express from "express";
+import { promises as fs } from "fs";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Endpoint to read and send JSON file content
+app.get("/socks", async (req, res) => {
+  try {
+    // Console log the entire request object
+    console.log(req);
+
+    const data = await fs.readFile("../data/socks.json", "utf8");
+    const jsonObj = JSON.parse(data);
+    res.json(jsonObj);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+  }
+});
+
+app.get("/socks/:color", async (req, res) => {
+  try {
+    // Console log the entire request object
+    console.log(req);
+    const { color } = req.params;
+    // Console log specific parts of the request
+    console.log("Req params - color:", color);
+
+    const data = await fs.readFile("../data/socks.json", "utf8");
+    const jsonObj = JSON.parse(data);
+
+    const correctSocks = jsonObj.filter((sock) => sock.color === color);
+
+    console.log("right socks?", correctSocks);
+
+    correctSocks.length === 0
+      ? res.status(404).send("No socks found with that color")
+      : res.json(correctSocks);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+  }
+});
+
+app.post("/socks", async (req, res) => {
+  try {
+    // Obligatory reference to POST Malone
+    console.log(
+      "If POST Malone were a sock, he'd be the one with the most colorful pattern."
+    );
+    // Simulate creating a user
+    const { username, email } = req.body;
+    if (!username || !email) {
+      // Bad request if username or email is missing
+      return res
+        .status(400)
+        .send({ error: "Username and email are required." });
+    }
+
+    // Respond with the created user information and a 201 Created status
+    res.status(201).send({
+      status: "success",
+      location: "http://localhost:3000/users/1234", // This URL should point to the newly created user
+      message: "User created successfully.",
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+  }
+});
+
+app.delete("/socks/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("Deleting sock with ID:", id);
+    res.status(200).send("Sock deleted successfully");
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send("Hmm, something doesn't smell right... Error deleting sock");
+  }
+});
+
+app.put("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email } = req.body;
+    console.log("Updating email for user with ID:", id);
+    res.status(200).send({
+      status: "success",
+      data: email, // This URL should point to the newly created user
+      message: "User updated successfully.",
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res
+      .status(500)
+      .send("Hmm, something doesn't smell right... Error deleting sock");
+  }
+});
+
+app.listen(PORT, function (err) {
+  if (err) console.log("Error in server setup");
+  console.log("Server listening on Port", PORT);
+});
